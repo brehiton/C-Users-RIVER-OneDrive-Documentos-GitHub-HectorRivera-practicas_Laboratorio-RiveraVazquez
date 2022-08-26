489f129c7b44bc4de0725f84c8ce857ba0ab4ca8
brehiton <102930052+brehiton@users.noreply.github.com>
2022-08-26 09:10:43 -0500

    practica02 8:46

---
+++ practica02/Saludador.java
@@ -0,0 +1,10 @@
+//inicio de la practica 7:55
+
+//Definicion de la clase
+public class Saludador{
+	//metodo para saludar
+	public void saludar(){ //llave que inicie el metodo
+		System.out.println("Hola Alumno de POO");
+	} //llave que cierre el metodo de saludar
+} //llave que cierra la clase
+
---
+++ practica02/TestSaludador.java
@@ -0,0 +1,11 @@
+// inicio de la practica 8:03
+
+public class TestSaludador{
+	//Metodo principal (main)
+	public static void main(String[] args){
+		//creando una instancia del Objeto Saludador
+		Saludador alumno = new Saludador();
+		//accediendo el metodo saludar del alumno
+		alumno.saludar();
+	}
+}
