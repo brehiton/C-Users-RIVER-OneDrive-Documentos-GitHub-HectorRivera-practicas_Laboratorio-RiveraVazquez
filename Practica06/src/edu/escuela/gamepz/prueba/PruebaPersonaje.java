337ffe43b03623fd498b0d1db0bd306f8367c378
brehiton <102930052+brehiton@users.noreply.github.com>
2022-09-09 11:01:27 -0500

    Practica06 11:01

--- Practica06/src/edu/escuela/gamepz/prueba/PruebaPersonaje.java
+++ Practica06/src/edu/escuela/gamepz/prueba/PruebaPersonaje.java
@@ -3,12 +3,19 @@ import edu.escuela.gamepz.personajes.*;
 
 public class PruebaPersonaje{
 	public static void main(String[] args){
-	Personaje alumno = new Personaje("Kal El");
+	Personaje alumno = new Personaje("Kal El",0);
 	alumno.saludar();
 	System.out.println("Nombre: "+ alumno.getNombre()+ "Edad: "+ alumno.getEdad());
 	System.out.println("Modificando el nombre" + alumno.getNombre());
 	alumno.setNombre("Michael jordan");
 	alumno.saludar();
+	//modificando
+	Personaje per01 = new Personaje("Almendra",200)
+	Personaje per01 = new Personaje("Ricardo",10)
+	Personaje per01 = new Personaje("Bianca",1)
+	Personaje per01 = new Personaje("Leonel",-10)
+	Personaje per01 = new Personaje("Hector",500)
+	System.out.println();
 	if (!alumno.setEdad(30)){
 		System.out.println("Edad sin cambios");
 		}
