f96fb3a76ae173b5c8cb32b2845b0ec8d61b9a3f
brehiton <102930052+brehiton@users.noreply.github.com>
2022-09-09 07:50:54 -0500

    Practica06 7:50

---
+++ Practica06/src/edu/escuela/personajes/Personaje.java
@@ -0,0 +1,32 @@
+package com.patito.poo1pract.personajes;
+
+public class Personaje{
+	private String nombre;
+	private int edad;
+
+	public Personaje(String nombre){
+		this.nombre = nombre;
+		this.edad = 0;
+	}
+	public int getEdad(){
+		return edad;
+	}
+
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public void saludar(){
+		System.out.println("Hola Alumno de POO" + nombre);
+	}
+	public boolean setEdad(int years){
+		boolean result = false;
+		if (years > 0 && years <121 ){
+			edad = years;
+			result = true;
+		}
+		return result;
+	}
+}
