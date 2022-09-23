e8c9f02f00efb9b09e00691bb1ef81e3807b11e4
brehiton <102930052+brehiton@users.noreply.github.com>
2022-09-23 08:00:50 -0500

    Practica07 8:00

--- Practica07/src/edu/escuela/gamepz/prueba/PruebaCiclos.java
+++ Practica07/src/edu/escuela/gamepz/prueba/PruebaCiclos.java
@@ -1,14 +1,19 @@
+package edu.escuela.gamepz.prueba;
+import edu.escuela.gamepz.personajes.*;
+
 public class PruebaCiclos{
 	public static void main(String[] args) {
-		Personaje per01 new Personaje("Diego");
-		Personaje per02 new Personaje("Almendra");
-		Personaje per03 new Personaje("Bianca");
-		Personaje pero04 new Personaje("Jordan");
+		Personaje per01 = new Personaje("Diego");
+		Personaje per02 = new Personaje("Almendra");
+		Personaje per03 = new Personaje("Bianca");
+		Personaje pero04 = new Personaje("Jordan");
 
 		int n = 0;
 		int m = 0;
-		while (n <= m){
-			n = (int)(Math.random.()*100);
+		while (n >= m){
+			n = (int)(Math.random()*100);
+			m = (int)(Math.random()*100);
 		}
+		System.out.println("n = "+n +" "+"m="+ m);
 	}
 }
