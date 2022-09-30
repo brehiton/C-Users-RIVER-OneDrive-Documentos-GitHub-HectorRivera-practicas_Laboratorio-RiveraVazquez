e21bd5ca01bcdf73af41df7ddb29d656cad5f52f
brehiton <102930052+brehiton@users.noreply.github.com>
2022-09-30 11:02:46 -0500

    Parcial1 11:00
    no supe de los primos por que no supe: como hacerlo y empezarlo :c

--- Parcial1/src/mx/parc1/soft/parcial/series/Serie.java
+++ Parcial1/src/mx/parc1/soft/parcial/series/Serie.java
@@ -4,12 +4,14 @@ public class Serie{
 	public int x;
 	public int y;
 	
+	 int numero = 0;
 	public static boolean primo(int m){
-		for(int i = 2; i < m; i ++)
+		for(int i = 1; i < numero; i ++)
+			m = (int)(Math.random()*10);
 			return true; 
-	}
+		}
 	public int genNumero(){
-		return 3;
-	} "hola"
+		return
+	}
 	
-}
+}     :,(
