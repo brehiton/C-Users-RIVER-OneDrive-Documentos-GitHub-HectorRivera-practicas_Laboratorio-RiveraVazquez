8e80673028e6df9d8220ff73187fd66ee4d1c82b
brehiton <102930052+brehiton@users.noreply.github.com>
2022-09-30 08:54:15 -0500

    Practica1 8:54

--- Parcial1/src/mx/parc1/soft/parcial/series/Serie.java
+++ Parcial1/src/mx/parc1/soft/parcial/series/Serie.java
@@ -2,7 +2,22 @@ package mx.parc1.soft.parcial.series;
 
 public class Serie{
 	public int primos;
+	public int primos2;
 	public Serie(int primos){
 		this.primos = primos;
+	}
+	public int getPrimos(){
+		return primos;
+	}
+	public boolean primos(int numeros){
+		boolean resultado = false;
+		if (numeros > 0 && numeros < 100){
+			primos = numeros;
+		}
+		return resultado;
+
+	}
+	public int genNumero(){
+		return primos2;
 	}
 }
