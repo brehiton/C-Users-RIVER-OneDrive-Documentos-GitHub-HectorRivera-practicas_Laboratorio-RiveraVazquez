32ca5fb1a9d2e3362e6cc1d57c0b263e38ad5f31
brehiton <102930052+brehiton@users.noreply.github.com>
2022-10-07 11:47:01 -0500

    Practica08 11:46

---
+++ Practica08/src/edu/escuela/gamepz/prueba/PruebaHerencia.java
@@ -0,0 +1,15 @@
+public class PruebaHerencia{
+	public static void main(String[] args) {
+		Personaje per01 new Personaje("David", 100);
+		Personaje per02 new Personaje("Bianca");
+		Planta plan01 new Planta("Fabian", 10, 'B');
+		Planta plan02 new Planta("Almendra", 50);
+		Planta plan03 new Planta("Ricardo", 'C');
+		Planta plan04 new Planta("Silvia");
+		Zombie zom02 new Zombie("Armando", 80, false);
+		Zombie zom02 new Zombie("Joselin", true);
+		Zombie zom02 new Zombie("Eduardo");
+
+		Personaje[] personajes = new Personaje[8];
+	}
+}
