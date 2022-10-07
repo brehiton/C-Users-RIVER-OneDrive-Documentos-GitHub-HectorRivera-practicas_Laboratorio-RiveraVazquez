0ead00d20e590b749d57828063318672ae4f2e3a
brehiton <102930052+brehiton@users.noreply.github.com>
2022-10-07 09:11:57 -0500

    Practica08 9:11

---
+++ Practica08/src/edu/escuela/gamepz/personajes/buenos/Planta.java
@@ -0,0 +1,39 @@
+package edu.escuela.gamepz.personajes.buenos;
+import edu.escuela.gamepz.personajes.Personaje;
+public class Planta extends Personaje{
+	private char escudo;
+
+	public Planta(String nombre, int vida, char escudo){
+		super(nombre, vida);
+		this.escudo = escudo;
+	}
+
+	public Planta(String nombre, char escudo){
+		this(nombre, 3, escudo );
+	}
+
+	public Planta(String nombre, int vida){
+		this(nombre, vida, 'A' );
+	}
+
+	public Planta(String nombre){
+		this(nombre, 3, 'A');
+	}
+
+	public char escudo(){
+		return escudo;
+	}
+
+	public char getDetalle(){
+		return nombre + "\t" + vida; 
+	}
+
+	public void decVida(){
+		vida * 2;
+	}
+
+	public void decVida(int resta){
+		resta = (int)(Math.random()*99);
+		vida -= resta;
+	}
+}
