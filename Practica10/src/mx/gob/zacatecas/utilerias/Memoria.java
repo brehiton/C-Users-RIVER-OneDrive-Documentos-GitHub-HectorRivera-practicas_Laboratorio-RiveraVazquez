14fb2d1ed03f1a5d605e6e67de3ebaa39ca57c03
brehiton <102930052+brehiton@users.noreply.github.com>
2022-10-21 08:02:41 -0500

    Practica10 8:02

---
+++ Practica10/src/mx/gob/zacatecas/utilerias/Memoria.java
@@ -0,0 +1,25 @@
+package 
+public enum Memoria{
+	DDR("memoriaRam", 1666f, 30),
+	DDR2("laptop", 240f, 35),
+	DDR3("laptop", 33f, 40),
+	DDR4("gamer", 166f, 40);
+	
+	private String tipo;
+	private int vel;
+	private float pines;
+
+	public int getDDR(){
+		return 1;
+	}
+	public int getDDR2(){
+		return 1;
+	}
+	public int getDDR3(){
+		return 1;
+	}
+	public int getDDR4(){
+		return 1;
+	}
+
+}
