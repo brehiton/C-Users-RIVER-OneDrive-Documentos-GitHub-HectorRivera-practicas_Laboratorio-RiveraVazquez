1228afaf9d483596192c663defe89df7cacc1410
brehiton <102930052+brehiton@users.noreply.github.com>
2022-10-21 08:16:01 -0500

    Practica10 8:15

---
+++ Practica10/src/mx/gob/zacatecas/utilerias/Tipo.java
@@ -0,0 +1,16 @@
+public enum Tipo{
+	SERVER("Servidor"),
+	WORKSTATION("Estacion de trabajo"),
+	DESKTOP("Escritorio"),
+	LAPTOP("Portatil");
+
+	public Tipo(){
+
+	}	
+	public String getSERVER(){
+		return 1;
+	}	
+	public String getWORKTATION(){
+		return 1;
+	}
+}
