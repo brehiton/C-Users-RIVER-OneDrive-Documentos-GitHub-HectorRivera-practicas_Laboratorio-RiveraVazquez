f0ca77031a8f846912edd3a48f21190151e2c327
brehiton <102930052+brehiton@users.noreply.github.com>
2022-10-28 10:51:51 -0500

    Practica11 10:51

--- Practica11/src/edu/escuela/gamepz/prueba/PruebaHerencia.java
+++ Practica11/src/edu/escuela/gamepz/prueba/PruebaHerencia.java
@@ -2,13 +2,14 @@ package edu.escuela.gamepz.prueba;
 import edu.escuela.gamepz.personajes.*;
 import edu.escuela.gamepz.personajes.buenos.*;
 import edu.escuela.gamepz.personajes.malos.*;
+import edu.escuela.gamepz.utils.*;
 public class PruebaHerencia{
 	public static void main(String[] args) {
-		Personaje per01 = new Personaje("David", 100);
-		Personaje per02 = new Personaje("Bianca");
-		Planta plan01 = new Planta("Fabian", 10, 'B');
+		Personaje per01 = new Planta("David", 100);
+		Personaje per02 = new Zombie("Bianca");
+		Planta plan01 = new Planta("Fabian", 10, Escudo.MEDIO);
 		Planta plan02 = new Planta("Almendra", 50);
-		Planta plan03 = new Planta("Ricardo", 'C');
+		Planta plan03 = new Planta("Ricardo", Escudo.BAJO);
 		Planta plan04 = new Planta("Silvia");
 		Zombie zom01 = new Zombie("Armando", 80, false);
 		Zombie zom02 = new Zombie("Joselin", true);
