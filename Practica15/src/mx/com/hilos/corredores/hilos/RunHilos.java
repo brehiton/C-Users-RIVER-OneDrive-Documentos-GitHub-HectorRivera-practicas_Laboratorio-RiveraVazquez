5968398342fe91bf3e98b77f2afacda72eff5cdc
brehiton <102930052+brehiton@users.noreply.github.com>
2022-11-25 11:49:48 -0600

    Practica15 11:49

--- Practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ Practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -1,11 +1,15 @@
+package mx.com.corredores.hilos;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 public class RunHilos{
 	public void iniciar(JTextField txt1, JTextField txt2, JLabel elem1, JLabel elem2){
-		char ch1 = txt1.getText().charAt(0);
-		char ch2 = txt2.getText().charAt(0);
+		char ch1;
+		char ch2;
 
-		if(txt1.getText().length() > 0 || txt1.getText().charAt(0) == " "){
+		String salida1 = String.valueOf(ch1);
+		String salida2 = String.valueOf(ch2);
+
+		if(ch1.length() > 0 || txt1.getText().charAt(0) == " "){
 			txt1.getText() = ch1;
 		} else{
 			txt1 = 'E';
@@ -13,7 +17,5 @@ public class RunHilos{
 		if(ch2 == null || ch1.length() == null){
 			ch2 = 'M';
 		}
-		String salida1 = String.valueOf(ch1);
-		String salida2 = String.valueOf(ch2);
 	}
 }
