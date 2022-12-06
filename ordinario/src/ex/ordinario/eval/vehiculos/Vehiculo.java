a7778ab236b91ace0862c05a0a09b5b266ae7700
brehiton <102930052+brehiton@users.noreply.github.com>
2022-12-06 09:09:22 -0600

    ordinario 9:09

--- ordinario/src/ex/ordinario/eval/vehiculos/Vehiculo.java
+++ ordinario/src/ex/ordinario/eval/vehiculos/Vehiculo.java
@@ -1,4 +1,6 @@
-public class Vehiculo {
+package ex.ordinario.eval.vehiculos;
+import java.util.Comparator;
+public class Vehiculo implements Comparator<Vehiculo>{
 	private String marca;
 	private int modelo;
 	private float costo;
@@ -20,5 +22,14 @@ public class Vehiculo {
 	}
 	public float getCosto(){
 		return costo;
+	}
+	public int compare(Vehiculo v1, Vehiculo v2){
+		if ((v1.marca.compareTo(v2.marca)) != 0){
+			return (v1.marca.compareTo(v1.marca));
+		}
+		if (v1.modelo != v2.modelo){
+			return (v2.modelo>v1.modelo)?(1):(-1);
+		}
+		return (v2.costo > v1.costo)?(1):(-1);
 	}
 }
