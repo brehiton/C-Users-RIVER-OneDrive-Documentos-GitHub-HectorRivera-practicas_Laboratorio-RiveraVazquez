f119aff6ca4f98437391385c07068a613483a31d
brehiton <102930052+brehiton@users.noreply.github.com>
2022-12-06 10:07:44 -0600

    ordinario 10:07

--- ordinario/src/ex/ordinario/eval/acciones/PorCosto.java
+++ ordinario/src/ex/ordinario/eval/acciones/PorCosto.java
@@ -2,4 +2,13 @@ package ex.ordinario.eval.acciones;
 import java.util.Comparator;
 import ex.ordinario.eval.vehiculos.*;
 public class PorCosto implements Comparator<Vehiculo>{
+	public int compare(Vehiculo v1, Vehiculo v2){
+		if (v1.getCosto() != v2.getCosto()){
+			return (v2.getCosto()>v1.getCosto())?(1):(-1);
+		}
+		if (v1.getModelo() != v2.getModelo()){
+			return (v2.getModelo()>v1.getModelo())?(1):(-1);
+		}
+		return (v1.getMarca().compareTo(v1.getMarca()));
+	}
 }
