84fc83ed8158846918e0dfcc75087e4672faa1f5
brehiton <102930052+brehiton@users.noreply.github.com>
2022-12-06 09:22:05 -0600

    ordinario 9:22

--- ordinario/src/ex/ordinario/eval/vehiculos/Acuatico.java
+++ ordinario/src/ex/ordinario/eval/vehiculos/Acuatico.java
@@ -1,3 +1,13 @@
-public class Acuatico{
+package ex.ordinario.eval.vehiculos;
+
+public class Acuatico extends Vehiculo{
 	boolean remos;
+
+	public Acuatico(String marca, int modelo, float costo, boolean remos){
+		super(marca, modelo, costo);
+		this.remos = remos;
+	}
+	public String toString(){
+		return super.toString() +"\t"+ remos;
+	}
 }
--- ordinario/src/ex/ordinario/eval/vehiculos/Terrestre.java
+++ ordinario/src/ex/ordinario/eval/vehiculos/Terrestre.java
@@ -1,3 +1,14 @@
-public class Terrestre{
+package ex.ordinario.eval.vehiculos;
+
+public class Terrestre extends Vehiculo{
 	int llantas;
+
+	public Terrestre(String marca, int modelo, float costo, int llantas){
+		super(marca, modelo, costo);
+		this.llantas = llantas;
+	}
+
+	public String toString(){
+		return super.toString() +"\t"+llantas;
+	}
 }
